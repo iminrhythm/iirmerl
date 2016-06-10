4970ed18968699d8cedcb011dfff884af6a1db9d
p r a x e p r a x i s <praxa@liervet.is>
2016-06-10 01:21:33 -0400

    Create p2y.py

---
+++ p2y.py
@@ -0,0 +1,3 @@
+python -c "from pyalgotrade.tools import yahoofinance; yahoofinance.download_daily_bars('dia', 2009, 'dia-2009.csv')"
+python -c "from pyalgotrade.tools import yahoofinance; yahoofinance.download_daily_bars('dia', 2010, 'dia-2010.csv')"
+python -c "from pyalgotrade.tools import yahoofinance; yahoofinance.download_daily_bars('dia', 2011, 'dia-2011.csv')"
